35c0ca4109ec440fe89d744b46fcbb7e05d2e3eb
arahatashun <arahatashun@gmail.com>
2018-02-24 19:47:59 +0900

    tura

--- 8_2/ekf.py
+++ 8_2/ekf.py
@@ -14,7 +14,8 @@ from numpy.linalg import norm
 from scipy.optimize import minimize
 from matplotlib.patches import Ellipse
 from math import atan2
-
+from scipy.linalg import eigh
+from sys import exit
 
 class EKF():
     """Extended Kalman Filter"""
@@ -103,6 +104,16 @@ class EKF():
         res = minimize(func, x0, method='Powell')
         return res.x
 
+def isPSD(A, tol=1e-8):
+    """ check if A is positive semi definite 
+    
+    https://stackoverflow.com/questions/5563743/check-for-positive-or-semi-positive-definite-matrix
+    :param A:  
+    :return: 
+    """
+    
+    E,V = eigh(A)
+    return np.all(E > -tol)
 
 def makefig1(x, esti, simp):
     """make figure comparing simple algorithm and extended karman filter
@@ -143,15 +154,16 @@ def makefig2(x, esti, var):
     eigen_vectors = []
     eigen_values = []
     for i in range(len(var)):
-        eigen_value, eigen_vector = np.linalg.eig(var[i])  # eigen value and eigen vector
+        eigen_value, eigen_vector = eigh(var[i],check_finite=True)  # eigen value and eigen vector
         eigen_values.append(eigen_value)
         eigen_vectors.append(eigen_vector)
-        # print("value", eigen_value)
+        print("value", eigen_value)
+        print(eigen_vector)
         # print("vector", eigen_vectors)
 
     eigen_vectors_arranged = [[[eigen_vectors[i][0][j], eigen_vectors[i][1][j]]
                                for j in range(2)] for i in range(len(var))]
-    print(eigen_values)
+    print("eigen_values", eigen_values)
     ax_len = np.sqrt(5.991 * np.array(eigen_values))
     print(ax_len)
     tilt = np.rad2deg([atan2(eigen_vectors_arranged[i][0][1], eigen_vectors_arranged[i][0][0])
@@ -161,7 +173,7 @@ def makefig2(x, esti, var):
     larger_ax = lambda j: ax_len[j, 0] if ax_len[j, 0] > ax_len[j, 1] else ax_len[j, 1]
     smaller_ax = lambda j: ax_len[j, 0] if ax_len[j, 0] < ax_len[j, 1] else ax_len[j, 1]
     for i in range(len(var)):
-        ell = Ellipse(xy=esti[i,:], width=smaller_ax(i), height=larger_ax(i), angle=tilt[i])
+        ell = Ellipse(xy=esti[i, :], width=smaller_ax(i), height=larger_ax(i), angle=tilt[i])
         ell.set_facecolor('none')
         ax.add_artist(ell)
         print(smaller_ax(i))
@@ -173,6 +185,12 @@ def makefig2(x, esti, var):
 def main():
     q = np.array([[3, 0], [0, 3]])
     r = np.array([[20, 10, 10], [10, 20, 10], [10, 10, 20]])
+    if not isPSD(q):
+        print("q is not positive definite")
+        exit()
+    elif not isPSD(r):
+        print("r is not positive definite")
+        exit()
     filter = EKF(q, r)
     T = 100  # sec
     coord = np.array([0, 0])
@@ -183,7 +201,7 @@ def main():
     for i in range(10):
         coord = coord + EKF.u + multivariate_normal([0, 0], filter.q)
         coords.append(coord)
-        y = EKF.getdistance(coord) + multivariate_normal([0, 0, 0], r)
+        y = EKF.getdistance(coord) + multivariate_normal([0, 0, 0], filter.r)
         # Extended Kalman Filter
         filter.prediction()
         mu, var = filter.update(y)
@@ -192,8 +210,10 @@ def main():
         # Simple Estimation
         simple_est = EKF.simple_estimation(y)
         simple_estimation.append(simple_est)
-    print(variance)
-    # makefig2(np.array(coords), np.array(estimation), np.array(variance))
+        if not isPSD(variance):
+            print("error")
+    print("variance",variance)
+    makefig2(np.array(coords), np.array(estimation), np.array(variance))
     # makefig1(np.array(coords), np.array(estimation), np.array(simple_estimation))
 
 
