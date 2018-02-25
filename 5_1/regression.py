e200f07890fc709dbc8e2f2e55e8947688dad84a
arahatashun <arahatashun@gmail.com>
2018-02-25 14:15:09 +0900

    tura

--- 5_1/regression.py
+++ 5_1/regression.py
@@ -22,6 +22,18 @@ def aic(q, n, k):
     return aic
 
 
+def bic(q, n, k):
+    """Bayesian information criterion
+
+    :param q:the sum of the squared error
+    :param n:number of data
+    :param k:number of parameter
+    :return:BIC
+    """
+    bic = n * log(q / n) + k * log(n)
+    return bic
+
+
 def read_data():
     """read data from mazdas.txt .
 
@@ -69,23 +81,25 @@ def plot_poly_fit(years, prices, order):
     coefficient = np.polyfit(years, prices, order)
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    ax.scatter(years, prices)
+    ax.scatter(years, prices, s=10)
     ax.set_xlabel("Year")
     ax.set_ylabel("Price")
     expression_string = "y="
-    for i in range(order + 1):
-        expression_string += "+" + str(coefficient[i]) + "$x^" + str(i) + "$\n"
     x_min = min(years)
     x_max = max(years)
     x = np.linspace(x_min, x_max, 100)
-    ax.plot(x, np.polyval(coefficient, x), label=expression_string)
+    ax.plot(x, np.polyval(coefficient, x))
+    for i in range(order + 1):
+        expression_string += "+" + str(coefficient[i]) + "x^" + str(order - i) + "\n"
     ax.set_title("order" + str(order) + " regression")
-    ax.legend(loc="upper left")
     sum_squared_error = sum([(prices[i] - np.polyval(coefficient, years[i])) ** 2 for i in range(len(years))])
     AIC = aic(sum_squared_error, len(years), order)
-    ax.text(72, 10000, "AIC:" + str(AIC))
-    # plt.show()
-    # plt.savefig("aic" + str(order)+"_poly.pgf")
+    BIC = bic(sum_squared_error,len(years), order)
+    print("AIC&",AIC,"\\")
+    print("BIC&",BIC,"\\")
+    print(expression_string)
+    #plt.show()
+    plt.savefig(str(order)+"_poly.pgf")
     return sum_squared_error
 
 
@@ -103,7 +117,7 @@ def calc_loo(years, prices, n):
         tmp_prices.pop(i)
         coefficient = np.polyfit(tmp_years, tmp_prices, n)
         squared_error += (prices[i] - np.polyval(coefficient, years[i])) ** 2 / len(prices)
-    print(squared_error)
+    print("LOO&",squared_error,"\\")
     return squared_error
 
 
@@ -121,6 +135,7 @@ def print_loo_list(years, prices, highest_order):
 
 if __name__ == '__main__':
     years, prices = read_data()
-    n = input("order")
-    print_loo_list(years, prices, n)
+    n = int(input("order"))
+    calc_loo(years, prices, n)
+    plot_poly_fit(years,prices,n)
     # plot_figure(years, prices)
