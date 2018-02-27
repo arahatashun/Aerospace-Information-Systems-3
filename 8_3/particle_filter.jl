193a549625ac518e82eef545fd3780804af881eb
arahatashun <arahatashun@gmail.com>
2018-02-28 04:39:57 +0900

    tura

--- 8_3/particle_filter.jl
+++ 8_3/particle_filter.jl
@@ -4,6 +4,8 @@
 # Particle Filter for Lorenz Equation
 using PyPlot
 using Distributions
+using Calculus
+
 const σ=10
 const ρ=28
 const β=8/3
@@ -16,6 +18,10 @@ const STD = 2.0
 const R = [STD 0 0;
             0 STD 0;
             0 0  STD]
+const Q = [STD 0 0;
+            0 STD 0;
+            0 0 STD]
+
 function rand_normal(μ, σ)
     #= return a random sample from a normal (Gaussian) distribution
     refering from
@@ -65,6 +71,39 @@ function runge_kutta(f, x, step)
     return sum
 end
 
+mutable struct EKF
+    #Extended Kalman Filter
+    state::Array{Float64,1}
+    variance::Array{Float64,2}
+end
+
+function predict!(filter::EKF)
+    filter.state += runge_kutta(Lorenz,filter.state,STEP)
+    V = filter.variance
+    Φ(x,y,z) = expm( [-σ σ 0;ρ-z -1 0 ;y 0 -β]*STEP)
+    a11 = derivative(x -> Φ(x,filter.state[2],filter.state[3])[1,1], filter.state[1])
+    a12 = derivative(x -> Φ(x,filter.state[2],filter.state[3])[1,2], filter.state[1])
+    a13 = derivative(x -> Φ(x,filter.state[2],filter.state[3])[1,3], filter.state[1])
+    a21 = derivative(x -> Φ(filter.state[1],x,filter.state[3])[2,1], filter.state[2])
+    a22 = derivative(x -> Φ(filter.state[1],x,filter.state[3])[2,2], filter.state[2])
+    a23 = derivative(x -> Φ(filter.state[1],x,filter.state[3])[2,3], filter.state[2])
+    a31 = derivative(x -> Φ(filter.state[1],filter.state[2],x)[3,1], filter.state[3])
+    a32 = derivative(x -> Φ(filter.state[1],filter.state[2],x)[3,2], filter.state[3])
+    a33 = derivative(x -> Φ(filter.state[1],filter.state[2],x)[3,3], filter.state[3])
+    A = [ a11 a12 a13;a21 a22 a23;a31 a32 a33]
+    V = A*V*A'+Q
+    filter.variance = V
+end
+
+function update(filter::EKF, observation::Array{Float64,1})
+    # update and observe
+    C = [1 0 0;0 1 0; 0 0 1]
+    V = filter.variance
+    K = V * C * inv(C * V * C + R)
+    filter.state += K * (observation - C *filter.state)
+    filter.variance = (eye(3)-K * C) * V
+end
+
 mutable struct ParticleFilter
     weight::Array{Float64,1}
     particle::Array{Float64,2}
@@ -103,7 +142,6 @@ function evaluation!(filter::ParticleFilter,observation::Array{Float64,1})
         filter.weight[i] = 1/((2π)^(3/2)*(det(R))^(1/2)) * exp(exponent)
     end
     Σp = sum(filter.weight)
-    #println(Σp)
     filter.weight = filter.weight/Σp 
 end
 
@@ -112,42 +150,53 @@ function  main()
     x_true = Array{Float64, 2}(STEPNUM+1,3)
     pf_estimation = Array{Float64, 2}(STEPNUM+1,3)
     simple_estimation = Array{Float64, 2}(STEPNUM+1,3)
+    ekf_estimation = Array{Float64, 2}(STEPNUM+1,3)
     #initial
     x_true[1,:] = x_ini
     pf_estimation[1,:] = x_ini
     simple_estimation[1,:] = x_ini
-    filter = initialize_pf()
+    ekf_estimation[1,:] = x_ini
+    pf = initialize_pf()
+    ekf = EKF(x_ini,R) 
     for i in 1:STEPNUM
         x_true[i+1,:] = x_true[i,:] + runge_kutta(Lorenz,x_true[i,:],STEP)
         simple_estimation[i+1,:] = simple_estimation[i,:] + runge_kutta(Lorenz,simple_estimation[i,:],STEP)
-        predict!(filter)
-        pf_estimation[i+1, :] = getmean(filter)
+        predict!(pf)
+        predict!(ekf)
+        pf_estimation[i+1, :] = getmean(pf)
+        ekf_estimation[i+1, :] = ekf.state
         if i %OBSERVATION == OBSERVATION-1
         observe = x_true[i+1,:] + [rand_normal(0,STD) for j in 1:3]
-        evaluation!(filter,observe)
+        evaluation!(pf,observe)
+        update(ekf,observe)
         simple_estimation[i+1,:] = observe
         end
     end
-    plot_all(x_true,pf_estimation,simple_estimation)
+    plot_all(x_true,pf_estimation,simple_estimation,ekf_estimation)
 end
 
-function plot_all(x_true, x_pf,simple)
+function plot_all(x_true, x_pf,simple,ekf)
     time = [STEP*(j-1) for j in 1:STEPNUM+1]
     fig = figure()
     ax = gca(projection="3d")
-    ax[:plot](x_true[:,1],x_true[:,2],x_true[:,3])
-    ax[:plot](x_pf[:,1],x_pf[:,2],x_pf[:,3])
-    ax[:plot](simple[:,1],simple[:,2],simple[:,3])
+    ax[:plot](x_true[:,1],x_true[:,2],x_true[:,3],label ="true")
+    ax[:plot](x_pf[:,1],x_pf[:,2],x_pf[:,3],label="particle filter")
+    ax[:plot](simple[:,1],simple[:,2],simple[:,3],label="simple estimation")
+    ax[:plot](ekf[:,1],ekf[:,2],ekf[:,3],label="extended kalman filter")
+    legend(loc="right")
+    PyPlot.plt[:savefig]("pf.pgf")
     PyPlot.plt[:show]()
     fig2 = figure()
     ax =fig2[:add_subplot](2,1,1)
     ax[:plot](time,x_true[:,1],label="true")
     ax[:plot](time,x_pf[:,1],label="particle filter")
     ax[:plot](time,simple[:,1],label="simple")
+    ax[:plot](time,ekf[:,1],label="ekf")
     legend(loc="right")
     ax =fig2[:add_subplot](2,1,2)
     ax[:plot](time,x_true[:,1]-x_pf[:,1],label="pf error")
     ax[:plot](time,x_true[:,1]-simple[:,1],label="simple error")
+    ax[:plot](time,ekf[:,1]-simple[:,1],label="ekf error")
     legend(loc="right")
     PyPlot.plt[:show]()
 end
